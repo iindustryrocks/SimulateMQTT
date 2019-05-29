2019ec9f373428ad704aa27b09c7a827d343a7d3
Luís Carlos Teixeira <luislclt@users.noreply.github.com>
2019-05-30 00:23:14 +0100

    UPDATE delete_all, renamed postgres script

--- DockerContainers/DeleteScripts/delete_all.sh
+++ DockerContainers/DeleteScripts/delete_all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DeleteScripts/delete_Postgres.sh
+DeleteScripts/delete_postgres.sh
 DeleteScripts/delete_odoo.sh
 DeleteScripts/delete_node-red.sh
 DeleteScripts/delete_mosquitto.sh
