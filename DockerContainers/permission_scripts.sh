944ffc611a75e7c53f061faa5162cca12d8b9990
Luís Carlos Teixeira <luislclt@users.noreply.github.com>
2019-05-29 23:37:08 +0100

    UDPATE permission_scripts, rename scripts names & all scripts for all create/delete containers.

--- DockerContainers/permission_scripts.sh
+++ DockerContainers/permission_scripts.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-chmod +x CreateScripts/dockercreate_postgres.sh \
-    CreateScripts/dockercreate_odoo.sh \
-    CreateScripts/dockercreate_node-red.sh \
-    CreateScripts/dockercreate_mosquitto.sh \
+chmod +x CreateScripts/postgres.sh \
+    CreateScripts/odoo.sh \
+    CreateScripts/node-red.sh \
+    CreateScripts/mosquitto.sh \
+    CreateScripts/create_all.sh \
     DockerRun/dockerrun_postgres.sh \
     DockerRun/dockerrun_odoo.sh \
     DockerRun/dockerrun_node-red.sh \
@@ -12,6 +13,7 @@ chmod +x CreateScripts/dockercreate_postgres.sh \
     DeleteScripts/delete_Postgres.sh.sh \
     DeleteScripts/delete_node-red.sh \
     DeleteScripts/delete_mosquitto.sh \
+    DeleteScripts/delete_all.sh \
     LogsScripts/posgres_logs.sh \
     LogsScripts/odoo_logs.sh \
     LogsScripts/node-red_logs.sh \
