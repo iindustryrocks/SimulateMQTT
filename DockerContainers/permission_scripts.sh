87d9d582757f07620130fcd8823828bc11f66358
Luís Carlos Teixeira <luislclt@users.noreply.github.com>
2019-05-29 23:39:32 +0100

    UPDATE permission_scripts, some cleanUp.

--- DockerContainers/permission_scripts.sh
+++ DockerContainers/permission_scripts.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-chmod +x CreateScripts/postgres.sh \
-    CreateScripts/odoo.sh \
-    CreateScripts/node-red.sh \
-    CreateScripts/mosquitto.sh \
+chmod +x CreateScripts/create_postgres.sh \
+    CreateScripts/create_odoo.sh \
+    CreateScripts/create_node-red.sh \
+    CreateScripts/create_mosquitto.sh \
     CreateScripts/create_all.sh \
     DockerRun/dockerrun_postgres.sh \
     DockerRun/dockerrun_odoo.sh \
     DockerRun/dockerrun_node-red.sh \
     DockerRun/dockerrun_mosquitto.sh \
     DeleteScripts/delete_odoo.sh \
-    DeleteScripts/delete_Postgres.sh.sh \
+    DeleteScripts/delete_Postgres.sh \
     DeleteScripts/delete_node-red.sh \
     DeleteScripts/delete_mosquitto.sh \
     DeleteScripts/delete_all.sh \
