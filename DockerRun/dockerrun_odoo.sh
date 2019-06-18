bb3d4947da33a39ba4b8fde5c3d12787462ea425
Luís Carlos Teixeira <luislclt@users.noreply.github.com>
2019-06-18 18:04:01 +0100

    UPDATE odoo dockerrun, add postgress_password, to ensure connection.

--- DockerRun/dockerrun_odoo.sh
+++ DockerRun/dockerrun_odoo.sh
@@ -6,5 +6,5 @@ docker rm odoo
 docker run -d --name odoo --link postgres-db:db -p 8069:8069 \
   -v odoo-data:/var/lib/odoo \
   -v odoo-extra-addons:/mnt/extra-addons \
-  --env POSTGRES_DB=postgres \
+  --env POSTGRES_DB=postgres --env POSTGRES_PASSWORD=odoo-pw2019 \
   library/odoo:12.0
